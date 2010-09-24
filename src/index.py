582eca7d8eb2198891ed21c444ab1b60f5ce76bb
ibagrak <ilya.bagrak@gmail.com>
2010-09-24 08:57:16 +0400

    Hello, world!

--- src/index.py
+++ src/index.py
@@ -6,21 +6,22 @@ Created on Sep 23, 2010
 __author__ = "Ilya Bagrak <ilya.bagrak@gmail.com>"
 
 import logging
-
+import os
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp.RequestHandler):
     
     def get(self, *args):
-        pass
+        path = os.path.join(os.path.dirname(__file__), "index.html")     
+        self.response.out.write(template.render(path, {}))
               
 application = webapp.WSGIApplication([
-                                      ('/',                                   MainPage),
-                                     ], debug=True)
+                                      ('/',                 MainPage),
+                                     ], debug = True)
 
-           
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
     run_wsgi_app(application)
