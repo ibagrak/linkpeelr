11b8c4fecf8a1b705bdb2d479efa16840735e3f0
Ilya Bagrak <ilya.bagrak@gmail.com>
2013-01-10 00:00:14 -0500

    Initial boostrap-based layout for new linkpeelr page.

--- gae/handlers/index.py
+++ gae/handlers/index.py
@@ -1,8 +1,4 @@
 import common
-import logging
-from core import model
-
-from google.appengine.ext import db
 
 class Index(common.BaseHandler):
     
@@ -10,16 +6,4 @@ class Index(common.BaseHandler):
         # demonstrates how session state can be changed        
         self.session_inc_pageviews()
         
-        self.prep_html_response('index.html', 
-                                { 'pageviews' : self.session['pageviews'], 
-                                'widgets' : db.Query(model.Widget).order('-__key__').fetch(5), 
-                                'form' : model.generate_model_form(model.Widget)})
-
-
-class WithLogin(common.BaseHandler):
-	@common.with_login
-	def get(self):
-		self.prep_html_response('index.html', 
-                                { 'pageviews' : self.session['pageviews'], 
-                                'widgets' : db.Query(model.Widget).order('-__key__').fetch(5), 
-                                'form' : model.generate_model_form(model.Widget)})
+        self.prep_html_response('index.html', {})
